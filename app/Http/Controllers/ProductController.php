331307f333087a12d59db2439d2b3cafc6335e77
Sagar Chauhan <sagar.chauhan@applie.in>
2022-11-07 16:52:54 +0530

    initial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::latest()->paginate(5);
+        return view("products.index", compact("products"))->with("i", (request()->input("page", 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("products.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required",
+            "detail" => "required",
+            "image" => "required|image|mimes:png,jpg,jpeg,gif,svg|max:2048"
+        ]);
+
+        // dd(public_path()."\images");
+
+        $input = $request->all();
+
+        if ($image = $request->file("image")) {
+            $destinationPath = public_path()."\images";
+            $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+
+        Product::create($input);
+
+        return redirect()->route("products.index")->with("success", "Product created successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return view("products.show", compact("product"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        return view("products.edit", compact("product"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            "name" => "required",
+            "detail" => "required"
+        ]);
+
+        $input = $request->all();
+
+        if ($image = $request->file("image")) {
+            $destinationPath = public_path()."\images";
+            $profileImage = date("YmdHis") . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] =  $profileImage;
+        } else {
+            unset($input['image']);
+        }
+
+        $product->update($input);
+
+        return redirect()->route("products.index")->with("success", "Product update successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route("products.index")->with("danger", "Product deleted successfully");
+    }
+}
